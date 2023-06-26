70f1e5cce0480959a0fd3657bde70ef1cb298452
Gerald <adudagerald@gmail.com>
2023-06-26 06:43:09 +0300

    get all restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,9 @@
 class RestaurantsController < ApplicationController
+     ###show all restaurants
+     def index
+          restaurants = Restaurant.all
+          render json: restaurants 
+     end
+
+     
 end
