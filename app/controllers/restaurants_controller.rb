6e190b845b99f3c181e9311ea8afa61f27172bc1
Gerald <adudagerald@gmail.com>
2023-06-26 06:47:44 +0300

    get restaurant by id

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,5 +5,13 @@ class RestaurantsController < ApplicationController
           render json: restaurants 
      end
 
-     
+     ###get reastaurant by id
+     def show 
+          restaurants = Restaurant.find_by(id: params[:id])
+          if restaurants
+               render json: restaurants
+          else
+               render json: {error:"Restaurant not found"}
+          end
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
 
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :show]
 end
